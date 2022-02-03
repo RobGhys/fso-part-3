2058c6bc8784cec41609b0e1012e6bce9d217a22
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-02-03 13:17:48 +0100

    error handling

--- index.js
+++ index.js
@@ -32,9 +32,18 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note);
-    })
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            response.status(400).send({ error: 'malformatted id'})
+        })
 })
 
 /****************************
