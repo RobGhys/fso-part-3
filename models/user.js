df0ce56f64a2073f4e17d428697f5a6e40dc7684
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-02-03 21:07:42 +0100

    fixed the issue that was link to the mongoose unique validator plugin (deprecated)

--- models/user.js
+++ models/user.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+/*const uniqueValidator = require('mongoose-unique-validator')*/
 
 const userSchema = new mongoose.Schema({
     username: {
      type: String,
-     unique: true
     },
     name: String,
     passwordHash: String,
@@ -17,7 +16,9 @@ const userSchema = new mongoose.Schema({
     ],
 })
 
+/*
 userSchema.plugin(uniqueValidator)
+*/
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
