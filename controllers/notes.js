cf92595e19fc6262ba246361f0588bb4f5819ccf
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-02-03 20:40:52 +0100

    issue with nesting between notes and users

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 /****************************
  *          GET             *
@@ -23,14 +24,22 @@ notesRouter.get('/:id', async (request, response, next) => {
  ***************************/
 notesRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const user = await User.findById(body.userId)
 
     const note = new Note({
         content: body.content,
-        important: body.important || false,
-        date: new Date()
+        important: body.important === undefined ? false : body.important,
+        date: new Date(),
+        user: user._id
     })
 
+    // Save in notes
     const savedNote = await note.save()
+
+    // Save in users
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.json(savedNote.toJSON())
 })
 
